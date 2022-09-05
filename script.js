a866f18002c0fa6c4a55d1bec543bcf71b5eedaa
Junia Uni Umamtun Nysa <unijunia@gmail.com>
2022-09-05 11:36:49 +0700

    Background Slide

---
+++ script.js
@@ -0,0 +1,13 @@
+let background1 = () => {
+    document.querySelector('body').style.background = "url('image/snow-forest.jpg') center center / cover"; 
+  }
+  let background2 = () => {
+    document.querySelector('body').style.background = "url('image/spring-forest.jpg') center center / cover"; 
+  }
+  let background3 = () => {
+    document.querySelector('body').style.background = "url('image/summer-forest.jpg') center center / cover"; 
+  }
+  let background4 = () => {
+    document.querySelector('body').style.background = "url('image/autumn-forest.jpg') center center / cover"; 
+  }
+  
